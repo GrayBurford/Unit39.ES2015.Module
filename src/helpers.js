8a778e11a1ed4c3c771582e460e280a705375502
GrayBurford <GrayBurford@Gmail.com>
2023-06-24 11:32:10 -0400

    finished exercise

---
+++ src/helpers.js
@@ -0,0 +1,19 @@
+
+
+function choice (items) {
+    const rand = Math.floor(Math.random() * items.length);
+
+    return items[rand];
+};
+
+function remove (items, item) {
+    for (let i = 0; i < items.length; i++) {
+        if (items[i] === item) 
+        return [...items.slice(0, i), ...items.slice(i+1)];
+    }
+
+    return undefined;
+};
+
+
+export { choice, remove };
